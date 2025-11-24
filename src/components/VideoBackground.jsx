f065344b91157b7ac9d54edee3287ca576f55861
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-11-24 22:35:51 +0530

    Shifted functionality of VideoBackground to custom hook useMovieTrailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,20 +1,8 @@
-import React ,{ useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTrailer } from '../utils/movieSlice';
+import { useSelector } from 'react-redux';
+import useMovieTrailer from '../hooks/useMovieTrailer';
 const VideoBackground = ({id}) => {
-    const dispatch = useDispatch();
-    const trailer = useSelector(store=>store.movie?.trailer)
-    const getClips = async ()=>{
-        const data = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,API_OPTIONS)
-        const json = await data.json();
-        console.log(json);
-        const trailer = json.results.filter(video=>video?.type === "Trailer")
-        dispatch(addTrailer(trailer));
-    }
-    useEffect(()=>{
-        getClips();
-    },[])
+  const trailer = useSelector(store=>store.movie?.trailer)
+  useMovieTrailer(id);
   return (
     <div>
       <iframe width="560" height="315" src={"https://www.youtube.com/embed/dqolYtJGuf4?si=" + trailer?.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" ></iframe>
---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addTrailer } from "../utils/movieSlice";
+import { API_OPTIONS } from "../utils/constants";
+
+const useMovieTrailer = ({id})=>{
+    const dispatch = useDispatch();
+    //fetching the trailer video and updating the store with the trailer video data
+    const getClips = async ()=>{
+        const data = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,API_OPTIONS)
+        const json = await data.json();
+        console.log(json);
+        const trailer = json.results.filter(video=>video?.type === "Trailer")
+        dispatch(addTrailer(trailer));
+    }
+    useEffect(()=>{
+        getClips();
+    },[])
+}
+export default useMovieTrailer;
